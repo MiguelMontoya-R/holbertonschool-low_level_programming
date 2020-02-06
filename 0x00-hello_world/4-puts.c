6846644c3d5a7f91e944c0c0708747eeefdb9b4c
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-02-06 01:09:15 +0000

    puts and comments added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/* entry point main, retunr 0*/
+/**
+   * main - Entry point
+    *
+     * Return: Always 0 (Success)
+      */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
