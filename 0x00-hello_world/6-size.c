bdc2acb74c817c9df2563e3856fb4b797d4ac77e
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-02-06 02:29:59 +0000

    order

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(charv));
 	printf("Size of an int: %lu byte(s)\n", sizeof(intv));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(doublev));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(longv));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longv));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(doublev));
 	printf("Size of a float: %lu byte(s)\n", sizeof(floatv));
 
 	return (0);
