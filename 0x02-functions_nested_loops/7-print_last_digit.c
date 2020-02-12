d9ed47b99df4ccf51f6eb36ee0ef49b0088a6ecc
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-02-12 02:14:24 +0000

    finally

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
  *print_last_digit - print last digit of n number
+ *
  *@n: integer number
  *Return: the last digit
  */
@@ -14,9 +15,10 @@ int print_last_digit(int n)
 	}
 	else
 	{
-		l = (n * (-1) % 10);
+		l = ((n * (-1)) % 10);
 	}
 	_putchar(l + '0');
+
 	return (l);
 }
 
