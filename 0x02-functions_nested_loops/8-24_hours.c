43613b721dcd01d5d5170f18ad8b5ec2f81b9468
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-02-12 04:44:58 +0000

    while nested loop

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/*
+ *jack_bauer - 24 hours
+ *
+ *Return: no return
+ */
+void jack_bauer(void)
+{
+	int hours, minutes;
+	int hours1, hours2, minutes1, minutes2;
+	
+	hours = 0;
+	while (hours < 24)
+	{
+		minutes = 0;
+		while (minutes < 60)
+		{
+			_putchar(hours / 10 + '0');
+			_putchar(hours % 10 + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes % 10 + '0');
+			_putchar('\n');
+			
+		minutes++;
+			
+		}
+		hours++;
+
+	}
+	
+	
+}
