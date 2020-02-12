4dab55bf830a06e1eabe456681ef1efeca002f66
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-02-12 23:20:46 +0000

    the easy one

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  *add - function to add two integers
+  *
+  *@i: First integer
+  *@j: Second integer
+  *Return: the result
+  */
+int add(int i, int j)
+{
+	int res;
+
+	res = i + j;
+
+	return (res);
+}
