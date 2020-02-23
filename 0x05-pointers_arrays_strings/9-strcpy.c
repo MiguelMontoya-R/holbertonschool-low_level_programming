3bf53fc65b09fb816c1009118266154d8f1eb50a
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-02-23 23:37:34 +0000

    just don't put always the star

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+  *_strcpy - copy string
+  *
+  *@dest: destination variable
+  *@src: source of variable
+  *Return: char*
+  */
+char *_strcpy(char *dest, char *src)
+{
+
+	char *strcpy = dest;
+
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	return (strcpy);
+}
+
+
+
+
