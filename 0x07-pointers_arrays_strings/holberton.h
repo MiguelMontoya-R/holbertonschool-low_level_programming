91287b1ee21eaa7e0f81d2507ced10ee1ce50df4
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-02-25 23:24:01 +0000

    work it

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
