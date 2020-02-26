8372405fa2d0519994e4eedd7912579091341527
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-02-26 19:06:29 +0000

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 /**
+ *_puts_recursion - function to recursively print all string using putchar that
+ * just admit 1 character
  *
+ *@s: pointer to the string
+ *Return: void
  */
 void _puts_recursion(char *s)
 {
