8e6039bd0b3482d5c72bbc39086440960424950c
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-02-28 14:44:02 +0000

    sqrt

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,9 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int is_prime_number(int n);
+int _pow_recursion(int x, int y);
+int _strlen_recursion(char *s);
 int factorial(int n);
 void _print_rev_recursion(char *s);
 int _putchar(char c);
 void _puts_recursion(char *s);
+int is_palindrome(char *s);
 
 #endif /*HOLBERTON_H*/
