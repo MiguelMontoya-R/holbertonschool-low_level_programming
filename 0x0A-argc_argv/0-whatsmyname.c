94ddc9b8cac89cf0dc2c5e928faba179e1406aaa
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-01 20:59:01 +0000

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ *main - program tells the name of itself
+ *
+ *@argc: argument counter
+ *@argv: string (pointer, array)
+ *Return: EXIT_SUCCESS
+ */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
