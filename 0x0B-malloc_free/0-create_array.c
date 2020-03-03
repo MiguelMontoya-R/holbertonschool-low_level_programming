4945a9ff305739067abb869f20ad507839444116
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-03 18:58:41 +0000

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *array;
 
-	if (size == 0)
+	array = malloc(size * sizeof(*array));
+	if (size == 0 || array == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
-		array = malloc(size * sizeof(*array));
 		/*creating the array character by character*/
 		while (i < size)
 		{
