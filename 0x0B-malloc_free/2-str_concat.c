a09cd911cde1bdd824475668bc3380d04b4a76a2
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-03 21:59:11 +0000

    sintaxis fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,28 +13,37 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, g, m, sum;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
-
+	}
 	while (s1[i] != '\0')
+	{
 		i++;
-
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-
+	}
 	while (s2[j] != '\0')
+	{
 		j++;
-
+	}
 	sum = i + j;
 	p = malloc(sizeof(*p) * (sum + 1));
 	if (p != NULL)
 	{
 		for (g = 0; g < i; g++)
+		{
 			p[g] = s1[g];
-
+		}
 		for (m = 0; m < sum; m++)
+		{
 			p[g++] = s2[m];
+		}
 	}
 	else
+	{
 		return (NULL);
+	}
 	return (p);
 }
