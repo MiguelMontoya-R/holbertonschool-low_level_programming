76afb2bb26899ba770de1cd6eb4536f1ef1bd7df
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-03 18:45:39 +0000

    try one

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *create_array - functions that creates an array
+ *
+ *@size: size of the array
+ *@c: character
+ *Return: NULL if size == 0 and return the pointer if is not
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *array;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		array = malloc(size * sizeof(*array));
+		/*creating the array character by character*/
+		while (i < size)
+		{
+			array[i] = c;
+			i++;
+		}
+	}
+	return (array);
+}
