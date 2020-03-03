bd9cbe6283a2420eac8655a1531262de504e8f72
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-03 20:14:37 +0000

    duplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,27 +10,28 @@ char *_strdup(char *str)
 	int i = 0;
 	char *p;
 
-	p = malloc(sizeof(*p) * i);
-
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	if (p == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		while (str[i] != '\0')
-		{
-			i++;
-		}
 
+	p = malloc(sizeof(*p) * (i + 1));
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (p != NULL)
+	{
 		for (j = 0; j < i; j++)
 		{
 			p[j] = str[j];
 		}
+	}
+	else if (p == NULL)
+	{
+		return (NULL);
 	}
 
 	return (p);
