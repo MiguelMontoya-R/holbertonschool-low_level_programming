eeff348daf756c09cf8c49ba41249ed865cf7237
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-03 21:27:52 +0000

    first try

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *str_concat - concatenates two strings
+ *
+ *@s1: string 1
+ *@s2: string 2
+ *Return: NULL for failure and pointer for success
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int i = 0, j = 0, g, m, sum;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	sum = i + j;
+	p = malloc(sizeof(*p) * (sum + 1));
+
+	if (p != NULL)
+	{
+		for (g = 0; g < i; g++)
+		{
+			p[g] = s1[g];
+		}
+		for (m = 0; m < sum; m++)
+		{
+			p[g++] = s2[m];
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (p);
+}
