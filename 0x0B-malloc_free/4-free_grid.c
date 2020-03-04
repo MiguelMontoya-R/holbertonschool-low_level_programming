2238f8a46591ed18fb2fdb23767263625a8d0915
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-04 05:11:49 +0000

    here we go

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- *
+ *free_grid - free the malloc reseved space
+ *@grid: grid we created
+ *@height: height
+ *Return: void
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	for(i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
