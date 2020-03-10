e14da26c7238704828581d09f90aa24b466a62d9
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-10 04:23:41 +0000

    Wanted to do with %.1f

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- *
+ * print_dog - print dog info
+ * @d: pointer to dog info
+ * Return: void
  */
 void print_dog(struct dog *d)
 {
 	if ((*d).name == NULL)
 	{
 		printf("Name: (nil)\n");
-		printf("Age: %.1f\n", (*d).age);
+		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
 	}
 	else if ((*d).owner == NULL)
 	{
 		printf("Name: %s\n", (*d).name);
-		printf("Age: %.1f\n", (*d).age);
+		printf("Age: %f\n", (*d).age);
 		printf("Owner: (nil)\n");
 	}
 	else if (d == NULL)
 	{
+		printf("\n");
 	}
 	else if (d)
 	{
 		printf("Name: %s\n", (*d).name);
-		printf("Age: %.1f\n", (*d).age);
+		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
 	}
 }
