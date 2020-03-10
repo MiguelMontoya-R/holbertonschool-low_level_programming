61c9ace457b855d2ad9cb2ac83d6c00c1d2f1eb5
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-10 01:34:46 +0000

    done it

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct of dog Django
+ * @name: name of the dog
+ * @owner: dog's owner
+ * @age: dog's age
+ * struct for dog info
+ */
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+
+#endif
