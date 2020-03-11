966c4ddc2aec60ecb87496f300047b943c91a039
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-11 19:39:43 +0000

    need help

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,9 @@
 #include "function_pointers.h"
 /**
- *
+ * print_name - print name
+ * @name: name
+¨* @f: pointer to function
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
