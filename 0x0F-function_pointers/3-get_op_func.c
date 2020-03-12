63827aab36be095a6372e3c805c74902cd56a5ff
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-12 20:04:29 +0000

    compare function to s

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,8 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "3-calc.h"
 /**
  *
  */
 int (*get_op_func(char *s))(int, int)
 {
-	if (
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (ops[i].op[0] == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
