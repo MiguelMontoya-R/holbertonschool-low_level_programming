0c22d16870463395183e67420c964b7003b9e577
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-12 03:37:56 +0000

    still doing it

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - addition
+ * op_sub - substraction
+ * op_mul - multiplication
+ * op_div - division
+ * op_mod - division reminder
+ * @a: number
+ * @b: number
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
