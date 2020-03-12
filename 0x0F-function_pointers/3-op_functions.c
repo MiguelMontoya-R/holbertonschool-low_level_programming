1d20f86833b47d35cada8a9b65bbef62cf223c44
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-12 20:49:41 +0000

    betty

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,25 +2,40 @@
 #include <stdlib.h>
 /**
  * op_add - addition
- * op_sub - substraction
- * op_mul - multiplication
- * op_div - division
- * op_mod - division reminder
  * @a: number
  * @b: number
+ * Return: a + b
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - substraction
+ * @a: number
+ * @b: number
+ * Return: a - b
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - multiplication
+ * @a: number
+ * @b: number
+ * Return: a * b
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - division
+ * @a: number
+ * @b: number
+ * Return: a / b
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -30,6 +45,12 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+/**
+ * op_mod - reminder
+ * @a: number
+ * @b: number
+ * Return: a % b
+ */
 int op_mod(int a, int b)
 {
 	if (b == 0)
