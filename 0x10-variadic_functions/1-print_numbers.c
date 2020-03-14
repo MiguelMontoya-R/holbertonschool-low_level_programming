ec60f6063fb4392bc8d3a799da1874e5c68a7dba
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-14 15:33:44 +0000

    Betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
+ * print_numbers - print numbers
+ * @separator: separator between numbers
+ * @n: number of numbers to print
  *
+ * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+/* Declare the list*/
 	va_list print_list;
 	unsigned int i = 0;
-
+/* Initialize the list*/
 	va_start(print_list, n);
-
+/* Print every element of the list*/
 	while (i < n)
 	{
 		printf("%d", va_arg(print_list, int));
