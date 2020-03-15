84549ffe496ba98277e6e33067eb0a2508f57ddd
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-15 22:04:28 +0000

    Fixing malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  *
+ * Return: pointer to new_dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int lname, lowner;
 
+	lname = strlen(name);
+	lowner = strlen(owner);
 	new_dog = malloc(sizeof(dog_t));
-	(*new_dog).name = name;
-	(*new_dog).age = age;
-	(*new_dog).owner = owner;
-	return (new_dog);
+
+
+	if (new_dog)
+	{
+
+		(*new_dog).name = malloc(sizeof(lname));
+		(*new_dog).age = age;
+		(*new_dog).owner = malloc(sizeof(lowner));
+		return (new_dog);
+	}
+	return (NULL);
 }
