e42e8402c61b8ae4ec696c42bfad897540d72f56
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-19 23:08:55 +0000

    first example

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints all elemnts of a list
+ * @h: pointer to list
+ * Return: if str NULL print (nil)
+ */
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
