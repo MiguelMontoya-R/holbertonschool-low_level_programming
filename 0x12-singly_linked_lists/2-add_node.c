5ec2f596bc424a84d945b37cd3f3a9dcd184effb
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-20 03:22:54 +0000

    betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (node == NULL)
 		return (NULL);
 	if (str)
-		for (counter = 0; str[counter]; counter++);
+		for (counter = 0; str[counter]; counter++)
+			;
 
 	node->len = counter;
 	node->str = strdup(str);
