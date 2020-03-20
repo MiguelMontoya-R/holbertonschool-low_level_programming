b4e0509f78aa518e001dd049e70d26071ecb35cd
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-20 03:54:23 +0000

    say it before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * before - prints beofre main
+ * @construct - attribute to print before main
+ * Return: void
+ */
+void before(void) __attribute__ ((constructor));
+
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
