2d1ee86f5dd8dd2c531c73cc92cc7d0040603f63
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-23 21:39:24 +0000

    without checker but working

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint - print all elements of linked list
+ * @h: pointer to head
+ * Return: void
+ */
+size_t print_listint(const listint_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
