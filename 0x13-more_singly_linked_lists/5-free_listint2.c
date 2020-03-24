13811be89142ee917e7a148c7dbe4ca1ffbad85c
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-24 18:42:54 +0000

    working

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: pointer to head
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	while (*head != NULL)
+	{
+		aux = *head;
+		*head = (**head).next;
+		free(aux);
+	}
+}
