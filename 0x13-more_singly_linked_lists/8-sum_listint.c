5948cf76f17fade2745c34baf4ff2f082ab02b1d
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-24 23:50:40 +0000

    working

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_listint - sum all list
+ * @head: pointer to list
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+	listint_t *tmp = head;
+
+	if (head == NULL)
+		return (0);
+
+	while (tmp)
+	{
+		sum += (*tmp).n; /* sum the data copied in tmp */
+		tmp = (*tmp).next; /*point tmp to the next node */
+	}
+	return (sum);
+}
