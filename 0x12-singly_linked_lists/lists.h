f5e7b05e0e5212e8ac0a3ea95e3ba769c1d7a667
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-24 03:07:52 +0000

    free_list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -16,6 +20,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+void free_list(list_t *head);
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
