ac8ec9b00d0522edbc2ac684aa3b63abc473dfcc
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-25 03:38:36 +0000

    Fixing if idx == 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,17 +22,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(listint_t)); /* allocate new node */
 
+	if (new_node == NULL)
+		return (NULL);
+
+	(*new_node).n = n; /* set new_node */
+
 	if (idx == 0)
 	{
 		(*new_node).next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	if (new_node == NULL)
-		return (NULL);
-
-	(*new_node).n = n; /* set new_node */
-
 	while (counter < idx - 1)
 	{
 		counter++;
