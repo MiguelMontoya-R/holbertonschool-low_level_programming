b1f827c5fe174837ba8c9104d594cd8e5e3daef6
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-25 03:47:52 +0000

    cleaning up coding and reusing some vars

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (tmp)
 	{
-		counter++;
+		counter++; /* getting linked list's lenght */
 		tmp = (*tmp).next;
 	}
 	if (idx > counter)
