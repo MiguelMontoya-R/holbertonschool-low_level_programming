b1adc3162e9a557114c3cf2ab46304387792ccc7
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-03-26 01:41:29 +0000

    Delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - delete node at given position
+ * @head: pointer to the linked list
+ * @index: Linked list's position to be deleted
+ * Return: 1 success / -1 fail
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *aux = *head; /* aux pointer to head */
+	unsigned int i = 0; /* counter */
+	listint_t *next; /* pointer to next node when needed */
+
+	if (*head == NULL)
+		return (-1); /* empty linked list */
+	if (index == 0)
+	{
+		*head = aux->next; /* head point to next node copied in aux */
+		free(aux); /* delete the old head a.k.a aux */
+		return (1);
+	}
+	while (i < index - 1)
+	{
+		aux = aux->next; /* moving in the linked list 'til get at ... */
+		i++; /*... the previous node (index - 1) */
+	}
+	if (aux == NULL || aux->next == NULL)
+		return (-1); /* if index is greater than the linked list */
+
+	next = aux->next->next;
+	free(aux->next);
+
+	aux->next = next;
+	return (1);
+}
