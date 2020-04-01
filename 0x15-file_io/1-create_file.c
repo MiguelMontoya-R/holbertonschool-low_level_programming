8d21a08ed0fce0e2b7e2b21f329260e313dc56f6
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-04-01 00:56:23 +0000

    while(text_content[lenght] && *(text_content + lenght))

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[lenght] && text_content)
+	while (text_content[lenght] && *(text_content + lenght))
 		lenght++;
 
 	err = write(fd, text_content, lenght);
