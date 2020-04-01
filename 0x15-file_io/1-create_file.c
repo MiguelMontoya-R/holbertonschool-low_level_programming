be07c733b644acec1a8aa945f77a1b2e79f61da6
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-04-01 00:49:17 +0000

    betty fixed and bugs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
  * create_file - create a file with specific permissions
+ * @filename: name of the file
  * @text_content: content of the text file
  * Return: 1 succed, -1 failed
  */
@@ -9,22 +10,20 @@ int create_file(const char *filename, char *text_content)
 	int fd, err, lenght = 0;
 
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
-		return(-1);
+		return (-1);
 
-	while(text_content[lenght])
+	while (text_content[lenght])
 		lenght++;
 
-	text_content[lenght] = '\0';
 	err = write(fd, text_content, lenght);
 	if (err == -1)
-		return(-1);
+		return (-1);
 
 	close(fd);
-
 	return (1);
 }
