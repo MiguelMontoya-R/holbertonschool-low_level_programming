2074557f84e3e5ed172cda4c51c442cd5d0c22ea
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-04-01 00:59:40 +0000

    if (text_content == NULL) added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[lenght] && *(text_content + lenght))
+	if (text_content == NULL)
+		return (1);
+
+	while (text_content[lenght])
 		lenght++;
 
 	err = write(fd, text_content, lenght);
