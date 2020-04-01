6316681f0da6a2ca62eebe3224442237ee022f55
Miguel Montoya Ramirez <miguelmr0510@gmail.com>
2020-04-01 00:30:33 +0000

    create_file(const cha *filename, char *text_content)

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
