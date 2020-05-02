102017e30fcf8037fa2b1101d31ed4617ca903f5
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-02 20:57:03 +0000

    are the same

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_dnodeint - add node at the biggining of doubly list
+ * @head: pointer to head
+ * @n: number to be added in the node
+ * Return: the address of the new element or NULL
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp = *head;
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = tmp;
+	*head = new_node;
+	return (new_node);
+}
