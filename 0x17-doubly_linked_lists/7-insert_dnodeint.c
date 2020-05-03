93b3a552360dcf5f5aa452335454e1b0730e734d
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-03 05:07:39 +0000

    Added new case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,11 +14,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	unsigned int counter = 0;
 
+	if (*h == NULL)
+	{
+		if (idx == 0)
+			return (add_dnodeint(h, n));
+		else
+			return (NULL);
+	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (idx == 0 || *h == NULL)
+	if (idx == 0)
 	{
 		free(new_node);
 		return (add_dnodeint(h, n));
