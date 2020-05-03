44a92a78e49c8e06480824f50ba6d2ed65955e4d
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-03 01:16:52 +0000

    changing while without if statement

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,12 +14,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	while (current && current->next)
+	while (current && counter < index)
 	{
-		if (counter > index)
-			return (current->prev);
 		current = current->next;
 		counter++;
 	}
-	return (NULL);
+	return (current);
 }
