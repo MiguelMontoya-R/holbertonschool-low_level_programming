95652a0f8aea37c5cd212991257abcf41154f5cc
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-03 04:29:39 +0000

    inside an else

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,18 +25,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new_node);
 		return (add_dnodeint(h, n));
 	}
-	while (current && counter < idx - 1)
+	else if (idx > 0)
 	{
-		if (current->next == NULL)
+		while (current && counter < idx - 1)
 		{
-			free(new_node);
-			return (NULL);
+			if (current->next == NULL)
+			{
+				free(new_node);
+				return (NULL);
+			}
+			current = current->next;
+			counter++;
 		}
-		current = current->next;
-		counter++;
+		new_node->prev = current;
+		new_node->next = current->next;
+		current->next = new_node;
+		return (new_node);
 	}
-	new_node->prev = current;
-	new_node->next = current->next;
-	current->next = new_node;
-	return (new_node);
+	return (NULL);
 }
