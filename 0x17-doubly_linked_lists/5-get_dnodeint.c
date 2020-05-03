4f8a44fb864cb1cf97175c0fba4330e232e58078
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-03 01:03:25 +0000

    Adding if statement

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *current = head;
 	unsigned int counter = 0;
 
-	if (head == NULL)
+	if (head == NULL || current == NULL)
 		return (NULL);
 	while (current->next)
 	{
