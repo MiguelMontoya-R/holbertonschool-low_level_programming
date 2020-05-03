021c25522a15e6bfcc2bb8f8e0912ed9375f98e4
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-03 23:36:55 +0000

    Chenage all perspective

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
+ * delete_node - delete node
+ * @head: doubly linked list
+ * @delete: node to be deleted
+ * Return: void
+ */
+void delete_node(dlistint_t **head, dlistint_t *delete)
+{
+	if (*head == NULL || delete == NULL)
+		return;
+	if (*head == delete)
+		*head = delete->next;
+	if (delete->next)
+		delete->next->prev = delete->prev;
+	if (delete->prev)
+		delete->prev->next = delete->next;
+	free(delete);
+}
+/**
  * delete_dnodeint_at_index - delete node at index
  * @head: pointer to head's list
  * @index: index to delete node
@@ -12,35 +30,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *delete = *head;
 	unsigned int counter = 0;
 
-	if (index == 0 && (*head)->next)
-	{
-		*head = (*head)->next;
-		delete->next->prev = *head;
-		free(delete);
+	if (*head == NULL)
 		return (1);
-	}
-	else if (index == 0 && (*head)->next == NULL)
-	{
-		(*head)->next = NULL;
-		return (1);
-	}
 	while (delete && counter < index)
 	{
-		if (delete->next->next == NULL && counter + 1 == index)
-		{
-			free(delete->next);
-			delete->next = NULL;
-			return (1);
-		}
-		else if (delete->next->next == NULL && counter + 1 != index)
-			return (-1);
 		delete = delete->next;
 		counter++;
 	}
-	if (delete->next == NULL)
+	if (delete == NULL)
 		return (-1);
-	delete->prev->next = delete->next;
-	delete->next->prev = delete->prev;
-	free(delete);
+	delete_node(head, delete);
 	return (1);
 }
