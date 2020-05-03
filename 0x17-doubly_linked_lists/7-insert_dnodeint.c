198b6d66ac091f49a5acf2c9598d3d488d99f3da
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-03 03:52:17 +0000

    Adding betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (current && counter < idx - 1)
 	{
 		if (current->next == NULL)
-				return (NULL);
+			return (NULL);
 		current = current->next;
 		counter++;
 	}
