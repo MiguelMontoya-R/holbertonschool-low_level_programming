6f939d70bbf691bc436291eb85e308c6d2d9e182
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-03 04:33:34 +0000

    removed if *h == NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	unsigned int counter = 0;
 
-	if (*h == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -25,22 +23,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new_node);
 		return (add_dnodeint(h, n));
 	}
-	else if (idx > 0)
+	while (current && counter < idx - 1)
 	{
-		while (current && counter < idx - 1)
+		if (current->next == NULL)
 		{
-			if (current->next == NULL)
-			{
-				free(new_node);
-				return (NULL);
-			}
-			current = current->next;
-			counter++;
+			free(new_node);
+			return (NULL);
 		}
-		new_node->prev = current;
-		new_node->next = current->next;
-		current->next = new_node;
-		return (new_node);
+		current = current->next;
+		counter++;
 	}
-	return (NULL);
+	new_node->prev = current;
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
 }
