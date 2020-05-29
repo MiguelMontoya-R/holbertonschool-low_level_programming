f95108a46434c176b06ecadf587065e424651c42
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-05-29 16:58:36 +0000

    One check wrong

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
+	{
+		free(new_table);
 		return (NULL);
+	}
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t) * size);
