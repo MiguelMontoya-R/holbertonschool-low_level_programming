60e53ab1a2f1bc434c9711e5231c9b98c2a3bed8
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-07-29 20:52:27 +0000

    Adding task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 /**
-  *_strncpy - copy a string 
-  *
+  *_strncpy - copy a string
   *@dest: destination pointer to the copied variable
   *@src: pointer to the variable which is gonna be copied
   *@n: n bytes are going to be copied
@@ -11,17 +10,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int counter = 0;
 
-	while (dest[counter] != '\0')
+	for (counter = 0; counter < n; counter++)
 	{
-		counter++;
-	}
-
-	int c = 0;
-
-	while (c < n && src[c] != '\0')
-	{
-		dest[counter] = src[c];
-		n++;
+		dest[counter] = src[counter];
 	}
 	return (dest);
 }
