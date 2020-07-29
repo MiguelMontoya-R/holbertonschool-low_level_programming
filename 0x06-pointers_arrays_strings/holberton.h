93d599ac81764d9121f2e7fae4c59a548d275db7
MiguelMontoya-R <miguelmr0510@gmail.com>
2020-07-29 21:01:19 +0000

    fixing bugs for copying emprty string

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,5 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
